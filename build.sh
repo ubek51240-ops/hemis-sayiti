ff8eff0f41248c5254cc8117708a0b28637e394c
ubek51240-ops <ubek51240@gmail.com>
2026-06-22 12:52:47 +0500

    Fix static storage and add auto user creation

--- build.sh
+++ build.sh
@@ -11,4 +11,9 @@ python manage.py collectstatic --no-input
 echo "Running database migrations..."
 python manage.py migrate
 
+echo "Creating default users..."
+export SUPERADMIN_PASSWORD="admin_password123!"
+export OPERATOR_PASSWORD="operator_password123!"
+python create_admin.py
+
 echo "Build finished!"
--- mini_hemis/settings.py
+++ mini_hemis/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
