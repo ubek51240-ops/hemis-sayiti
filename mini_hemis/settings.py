1f0acb353f7521da9502fb3e9ee298706aaf3884
ubek51240-ops <ubek51240@gmail.com>
2026-06-22 12:29:44 +0500

    fix: production settings and static files for render

--- mini_hemis/settings.py
+++ mini_hemis/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
